c7607b347e2a5aaf899404ed9cf311be41bb996d
Veena Subramaniyam <lveena1@LAMU02YJ4AEJGH6.uhc.com>
2019-06-04 17:03:21 -0700

    updated sh file

--- pre-build.sh
+++ pre-build.sh
@@ -1,4 +1,4 @@
-
+#!/bin/sh
 go get -u github.com/golang/protobuf/protoc-gen-go
 go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
 go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
